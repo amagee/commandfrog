19df75ed8857c79c2bb77f54c199d39035a35397
Andrew Magee <amagee@gmail.com>
2020-11-29 13:39:35 +1100

    added build script

---
+++ build/__init__.py
@@ -0,0 +1,24 @@
+import subprocess
+from pathlib import Path
+from functools import reduce
+import operator
+
+
+def build():
+    subprocess.run("rm -rf dist", cwd="commandfrog", check=True, shell=True)
+
+    operations = [p.stem for p in (Path("commandfrog") / "operations").glob("*.py")]
+
+    subprocess.run(
+        [
+            "pyinstaller",
+            *reduce(
+                operator.iconcat, 
+                [["--hidden-import", f"commandfrog.operations.{f}"] for f in operations]
+            ),
+            "-F",
+            "deploy.py",
+        ],
+        cwd="commandfrog",
+        check=True,
+    )
