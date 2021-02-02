d2cf7d47dcb2a6a7d060e63e48ae1b8c4eeea08b
Andrew Magee <amagee@gmail.com>
2021-02-02 15:25:16 +1100

    docker driver: expand tildes on putting files

--- commandfrog/drivers/docker.py
+++ commandfrog/drivers/docker.py
@@ -10,6 +10,7 @@ from loguru import logger
 from commandfrog.config import Config
 from commandfrog.drivers.driver import Driver
 from commandfrog.operations.files import directory
+from commandfrog.utils import set_env
 
 from .util import execute_command
 
@@ -36,6 +37,17 @@ class DockerHost(Driver):
             self.container_id = container_id
 
     def put(self, path: str, contents: Union[str, bytes, StringIO], mode: Optional[Union[str, int]] = None):
+        if "~" in path:
+            # If we have a tilde in the path, expand it to an absolute path
+            # using os.path.expanduser. But that function doesn't give you a
+            # direct way of saying what the user's home directory should be,
+            # except for setting the `HOME` environment variable, so we do
+            # that. We take the HOME environment variable from the container
+            # and use it while calling `os.path.expanduser`.
+            home_dir = self.base_exec("echo $HOME", echo_stdout=False).stdout.decode().strip()
+            with set_env(HOME=home_dir):
+                path = os.path.expanduser(path)
+
         directory(self, os.path.dirname(path))
         with tempfile.NamedTemporaryFile() as fp:
             if isinstance(contents, str):
@@ -49,13 +61,25 @@ class DockerHost(Driver):
             cmd = f"docker cp {fp.name} {self.container_id}:{path}"
             logger.debug("Executing command: {}", cmd)
             subprocess.run(cmd, shell=True, check=True)
-            self.exec(f"chmod {mode} {path}")
+            if mode is not None:
+                self.exec(f"chmod {mode} {path}")
 
-    def base_exec(self, cmd: str, assert_ok: bool = True):
+    def base_exec(
+        self,
+        cmd: str,
+        assert_ok: bool = True,
+        echo_stdout: bool = True,
+        echo_stderr: bool = True,
+    ):
         if self.container_id is None:
             raise ValueError("...")
         cmd = f"docker exec {self.container_id} sh -c {shlex.quote(cmd)}"
-        return execute_command(cmd, assert_ok=assert_ok)
+        return execute_command(
+            cmd,
+            echo_stdout=echo_stdout,
+            echo_stderr=echo_stderr,
+            assert_ok=assert_ok
+        )
 
     def commit(self) -> str:
         """
